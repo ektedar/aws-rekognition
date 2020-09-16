6df158addb2af3e6a6a1cc54b553ebf3fad94fd8
Ektedar Alamm <ektedar_alam@live.com>
2020-09-16 01:21:37 -0400

    Updates to include more information and modularize the code.

--- src/aws_rekognition.py
+++ src/aws_rekognition.py
@@ -1,6 +1,7 @@
 ## This code utilizes the boto3 rekognition packages to analyze IMAGES ONLY.
 import boto3
 import logging
+import argparse
 
 def detect_faces(bucketname, image):
     """
@@ -15,11 +16,32 @@ def detect_faces(bucketname, image):
             }
         },
         Attributes=[
-            'DEFAULT',
+            'ALL',
         ]
     )
     return response
 
 if __name__ == "__main__":
-    output = detect_faces('testing-aws-rekognition', 'KingInTheNorth.png')
+    parser = argparse.ArgumentParser(
+        description='''
+        This python file will return an dict for the facial detection that was run over the provided image. This is assuming your data exists in the provided S3 Bucket. 
+        This will print an dict with the facial results if called directly. Refer to boto3 AWS Rekognition for more info.
+        '''
+    )
+
+    parser.add_argument(
+        '-f',
+        '--filename',
+        required=True,
+        help='This is the filename of the object that you are trying execute facial detection on.'
+    )
+
+    parser.add_argument(
+        '-b',
+        '--bucketname',
+        required=True,
+        help='This is the bucket name of the S3 bucket where the file is residing'
+    )
+    args = parser.parse_args()
+    output = detect_faces(args.bucketname, args.filename)
     print(output)
