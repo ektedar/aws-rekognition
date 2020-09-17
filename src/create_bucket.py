b55a7145e3b194d0dd11361143e355da40fd3278
Ektedar Alamm <ektedar_alam@live.com>
2020-09-16 23:51:26 -0400

    Added Boto3 code to create a bucket if needed.

---
+++ src/create_bucket.py
@@ -0,0 +1,55 @@
+import logging
+import boto3
+import argparse
+from botocore.exceptions import ClientError
+
+
+def create_bucket(bucket_name, region=None):
+    """Create an S3 bucket in a specified region
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created, else False
+    """
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='This python file will upload a specified document into an AWS s3 bucket'
+    )
+
+    parser.add_argument(
+        '-r',
+        '--region',
+        required=False,
+        help='Region name of where the bucket will be created'
+    )
+
+    parser.add_argument(
+        '-b',
+        '--bucketname',
+        required=True,
+        help='This is the bucket name of the S3 bucket'
+    )
+    args = parser.parse_args()
+
+
+    ## Calling the main function to do the upload
+    create_bucket(args.bucketname, args.region)
